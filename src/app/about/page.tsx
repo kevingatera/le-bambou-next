1f2bcfe328e1af9f896818f34e786a6ed50e6328
Kevin Gatera <kevingatera@gmail.com>
2024-09-30 05:09:00 +0000

    Add closing remarks on about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,6 +10,8 @@ export default async function Home() {
         <AboutUsBannerSection />
         <div className="h-[120px] min-h-[7.5rem] bg-[#566c6a] py-[60px]"></div>
         <HistoryVisionValuesSection />
+
+        <div className="max-w-4xl mx-auto text-center text-3xl py-8 mb-12">Join us on a journey to create cherished moments in Rwanda's captivating landscapes.</div>
       </main>
     </HydrateClient>
   );
