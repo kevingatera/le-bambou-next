d0db342deda8ee07e81e08c56ed2e899fbd9887a
Kevin Gatera <kevingatera@gmail.com>
2024-10-02 01:31:51 +0000

    Fixed apostrophe that snuck into html text

--- src/app/_components/sections/OutsideAttractionsListSection.tsx
+++ src/app/_components/sections/OutsideAttractionsListSection.tsx
@@ -9,7 +9,7 @@ export const OutsideAttractionsListSection = () => {
           <div className="verticle-padding outside-the-park">
             <div className="attraction-max-width-heading">
               <h2 className="text-2xl md:text-3xl lg:text-4xl"><strong id="Outside-volcanoes-park" className="font-bold">Outside Volcanoes National Park</strong></h2>
-              <div className="py-8 text-sm md:text-base lg:text-lg">Beyond the Park's Borders: Discovering Exquisite Delights in the Surrounding Environs</div>
+              <div className="py-8 text-sm md:text-base lg:text-lg">Beyond the Park&apos;s Borders: Discovering Exquisite Delights in the Surrounding Environs</div>
             </div>
             <div className="grid gap-8 md:gap-12 lg:gap-16 bg-[#b9c5c4] rounded-lg p-4 md:p-6 lg:p-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
               {attractions.map((attraction, index) => (
