6c40091e87759a8547874043c322e2e5264f119b
Kevin Gatera <kevingatera@gmail.com>
2024-10-07 16:28:17 +0000

    Redirect /webmail to siteground webmail

--- next.config.js
+++ next.config.js
@@ -9,6 +9,25 @@ const config = {
   images: {
     imageSizes: [500, 800, 1080, 1600, 2000, 2600],
   },
+  async redirects() {
+    return [
+      {
+        source: '/webmail/mail',
+        destination: 'https://gtxm1298.siteground.biz/webmail/mail',
+        permanent: true,
+      },
+      {
+        source: '/webmail/login',
+        destination: 'https://gtxm1298.siteground.biz/webmail/login',
+        permanent: true,
+      },
+      {
+        source: '/webmail',
+        destination: 'https://gtxm1298.siteground.biz/webmail',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default config;
