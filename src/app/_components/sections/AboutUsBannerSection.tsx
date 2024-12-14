73380b8f3e566bbf3ba2ff87d84943db18d5a74b
Kevin Gatera <kevingatera@gmail.com>
2024-12-14 15:15:42 -0500

    Update AboutUsBannerSection and page metadata with new image assets and styles for improved aesthetics and accessibility

--- src/app/_components/sections/AboutUsBannerSection.tsx
+++ src/app/_components/sections/AboutUsBannerSection.tsx
@@ -6,16 +6,24 @@ export const AboutUsBannerSection = async () => {
     return (
         <section className="flex items-center min-h-[calc(100dvh-7rem)] relative bg-gray-700">
             <Image
-                src="/images/DSC_3494.jpg"
+                src="/images/rooms/lobby/View of the main lobby-house lit at night-2.webp"
                 alt="Background Image"
                 fill={true}
-                className=" opacity-70 object-cover"
-                quality={75}
+                className="opacity-50 object-cover"
+                quality={90}
                 priority={true}
-                blurDataURL={await dynamicBlurDataUrl("/images/DSC_3494.jpg")}
+                blurDataURL={await dynamicBlurDataUrl(
+                    "/images/rooms/lobby/View of the main lobby-house lit at night-2.webp",
+                )}
                 placeholder="blur"
             />
-            <div className="max-w-[calc(100vw-2rem)] mx-auto relative">
+            <div className="absolute inset-0 bg-[#2a3332] opacity-20 pointer-events-none 
+                before:absolute before:inset-0 
+                before:bg-[radial-gradient(rgba(86,108,106,0.1)_1px,transparent_1px)] 
+                before:bg-[length:16px_16px] 
+                before:opacity-50">
+            </div>
+            <div className="max-w-[calc(100vw-2rem)] mx-auto relative z-10">
                 <div className="max-w-[900px] text-center bg-[rgba(121,98,90,.89)] rounded-lg flex flex-col justify-center items-center py-4 px-3">
                     <h1 className="text-[#ebf8f7] mt-0 mb-8 pt-5 text-[38px] font-normal leading-[1.1]">
                         About Le Bambou Gorilla Lodge:
