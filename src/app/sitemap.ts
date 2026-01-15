e365afc262c3457ecbf0b0e371a55ed9a8fe6b66
Kevin Gatera <kevingatera@gmail.com>
2026-01-14 20:29:42 -0500

    Adjust Next.js 16 headers and types

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { headers } from 'next/headers'
 const BASE_URL = 'https://lebambougorillalodge.com'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const headersList = headers()
+  const headersList = await headers()
   const host = headersList.get('host') ?? BASE_URL
 
   // Static routes
